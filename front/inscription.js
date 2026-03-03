027520ed3d37ed49af78699d9e7081d647c5bed7
Raphael Bruchet <raphael.bruchet80800@gmail.com>
2026-03-03 09:47:21 +0100

    update ip code html

--- front/inscription.js
+++ front/inscription.js
@@ -1,4 +1,4 @@
-const API_URL = "http://172.29.160.1160/api";
+const API_URL = "http://172.29.160.160/api";
 
 async function register() {
     const prenom = document.getElementById("prenom").value.trim();
