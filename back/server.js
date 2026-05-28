f54ac063faf5472e1f142107af94220801205cae
DumeS0108 <dumesacha@gmail.com>
2026-05-28 15:05:27 +0200

    Retirage d'un const

--- back/server.js
+++ back/server.js
@@ -380,9 +380,6 @@ app.post('/api/refresh-token', (req, res) => {
 // 🌊 GESTION POSEIDON (ETUDIANT 2)
 // ========================================
 
-const IOPoseidon = require('./IOPoseidon'); // Vérifie le chemin si besoin
-const poseidon = new IOPoseidon('172.29.254.100'); // IP de la carte Poseidon
-
 async function startWaterSupervision() {
   try {
     await poseidon.connect();
